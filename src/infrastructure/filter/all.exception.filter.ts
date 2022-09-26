3546c9f263924be00997a80672e16bb7155bc75e
rojiwon0325 <bond9986@naver.com>
2022-09-26 18:36:15 +0900

    인프라 모듈 추가 #8 - 전역 validation pipe 추가 - 전역 예외필터 추가

---
+++ src/infrastructure/filter/all.exception.filter.ts
@@ -0,0 +1,61 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+
+type IResponse = {
+  statusCode: number;
+  message: string;
+};
+
+@Catch()
+export class AllExceptionFilter implements ExceptionFilter {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  catch(exception: unknown, host: ArgumentsHost) {
+    const { httpAdapter } = this.httpAdapterHost;
+    const ctx = host.switchToHttp();
+    let statusCode: number;
+    let responseBody: IResponse;
+    if (exception instanceof HttpException) {
+      statusCode = exception.getStatus();
+      responseBody = {
+        statusCode,
+        message: this.extractMessageInHttpException(exception),
+      };
+    } else {
+      statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+      responseBody = {
+        statusCode,
+        message: this.extractMessageInUnKnwon(exception),
+      };
+    }
+
+    httpAdapter.reply(ctx.getResponse(), responseBody, statusCode);
+  }
+  extractMessageInHttpException(exception: HttpException): string {
+    const exceptionResponse = exception.getResponse();
+    if (typeof exceptionResponse == 'object') {
+      const message = exceptionResponse['message'] ?? null;
+      if (typeof message == 'string') {
+        return message;
+      } else if (message.constructor === Array && message.length > 0) {
+        return message[0];
+      }
+    }
+    return exception.message;
+  }
+  extractMessageInUnKnwon(exception: unknown): string {
+    if (typeof exception === 'object' && 'message' in exception) {
+      const message = exception['message'];
+      if (typeof message == 'string') {
+        return message;
+      }
+    }
+    return '예상하지 못한 에러가 발생했습니다.';
+  }
+}
--- src/infrastructure/infrastructure.module.ts
+++ src/infrastructure/infrastructure.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
+import { AllExceptionFilter } from './filter/all.exception.filter';
+import { ValidationPipe } from './pipe/validation.pipe';
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [],
+  providers: [
+    { provide: APP_FILTER, useClass: AllExceptionFilter },
+    { provide: APP_PIPE, useClass: ValidationPipe },
+  ],
 })
 export class InfrastructureModule {}
