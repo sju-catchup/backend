90be2705122446b8bdf6045f4fbcdac5243d2c1e
rojiwon0325 <bond9986@naver.com>
2022-09-26 20:03:45 +0900

    typeorm 설정 환경변수 적용#8 - nest useFactory를 통해 configService 사용

--- src/infrastructure/DB/typeorm.module.ts
+++ src/infrastructure/DB/typeorm.module.ts
@@ -1,13 +1,18 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule as OriginalOrmModule } from '@nestjs/typeorm';
 
-export const TypeOrmModule = OriginalOrmModule.forRoot({
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'root',
-  database: 'development',
-  synchronize: true,
-  logging: true,
-  entities: [],
+export const TypeOrmModule = OriginalOrmModule.forRootAsync({
+  imports: [ConfigModule],
+  useFactory: async (configService: ConfigService<IEnv>) => ({
+    type: 'mysql',
+    host: configService.get('DB_HOST'),
+    port: parseInt(configService.get('DB_PORT')),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_DATABASE'),
+    synchronize: true,
+    logging: true,
+    entities: [],
+  }),
+  inject: [ConfigService],
 });
