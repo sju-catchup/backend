e1a98a8356af4342fdd59ae0e9b9fafb071d0640
rojiwon0325 <bond9986@naver.com>
2022-10-01 15:46:58 +0900

    repository interface 수정

--- src/api/cctv/domain/repository.interface.ts
+++ src/api/cctv/domain/repository.interface.ts
@@ -1,6 +1,4 @@
 import { IBaseRepository } from 'src/api/common/interface/repository.interface';
 import { ICCTVId, ICCTV } from './cctv.interface';
 
-export interface ICCTVRepository extends IBaseRepository<ICCTVId, ICCTV> {
-  findMany: () => Promise<ICCTV[]>;
-}
+export type ICCTVRepository = IBaseRepository<ICCTVId, ICCTV>;
