3bfd9de746e92e24bd47dd841fc55d8a806bafdb
rojiwon0325 <bond9986@naver.com>
2022-10-18 20:02:01 +0900

    relation id 누락 bugfix

--- src/api/humanAction/infrastructure/model/human-action.entity.ts
+++ src/api/humanAction/infrastructure/model/human-action.entity.ts
@@ -2,7 +2,7 @@ import { IsBoolean, IsDate, IsNumber, IsString, IsUrl } from 'class-validator';
 import { ICCTVId } from 'src/api/cctv/domain/cctv.interface';
 import { CCTVEntity } from 'src/api/cctv/infrastructure/model/cctv.entity';
 import { TypeOrmRootEntity } from 'src/api/common/entitiy.base';
-import { Column, Entity, ManyToOne, RelationId } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 
 @Entity()
 export class HumanActionEntity extends TypeOrmRootEntity {
@@ -22,11 +22,12 @@ export class HumanActionEntity extends TypeOrmRootEntity {
   @IsUrl()
   uri: string;
 
-  @RelationId((entity: HumanActionEntity) => entity.cctv)
+  @Column()
   @IsNumber()
   cctv_id: ICCTVId;
 
   @ManyToOne(() => CCTVEntity)
+  @JoinColumn({ name: 'cctv_id' })
   cctv: CCTVEntity;
 
   @Column({ default: true })
